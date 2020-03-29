085fe9a72e7cbaad1fd89c84bf9d45c3a4e75b89
phoenixbf <phoenixbf@gmail.com>
2020-03-29 15:25:58 +0200

    OpenBCI tool improvements

--- tools/openBCI/app-cyton.js
+++ tools/openBCI/app-cyton.js
@@ -1,11 +1,11 @@
 /*
-    PRISMIN Cyton Encoder tool
+    PRISMIN OpenBCI/Cyton Encoder tool
 
     Author: Bruno Fanini (bruno.fanini__AT__gmail.com)
 
 ==========================================================================*/
-const Cyton = require("@openbci/cyton");
-const Constants = require("@openbci/utilities").constants;
+const Cyton           = require("@openbci/cyton");
+const Constants       = require("@openbci/utilities").constants;
 const commandLineArgs = require('command-line-args');
 
 const QVOSP = require("./qvosp");
@@ -13,15 +13,15 @@ const QVOSP = require("./qvosp");
 const outFolder = __dirname+"/_OUT/";
 const APP_WRITE_INTERVAL = 4000; // millsec.
 
-let vmin = undefined;
-let vmax = undefined;
+let vRange = [undefined,undefined];
 let ti = 0;
+let port = Constants.OBCISimulatorPortName;
 
 
 const optDefs = [
     { name: 'channels', type: Number}, // no. channels
-//  { name: 'xxx', type: String}, // 
-    { name: 'sim', type: Boolean}   // Simulate
+    { name: 'port', type: String},  // Port name 
+//  { name: 'sim', type: Boolean}   // Simulate
 ];
 const inargs = commandLineArgs(optDefs);
 
@@ -34,8 +34,21 @@ const ourBoard = new Cyton({
 let vPrism = new QVOSP();
 vPrism.outfolder = outFolder;
 
+// If port provided connect to port, else try autofind
+if (inargs.port) port = inargs.port;
+else {
+    ourBoard.autoFindOpenBCIBoard().then(portName => {
+        if (portName){
+            port = portName;
+            }
+        else {
+            // Unable to auto find OpenBCI board
+            }
+        });
+}
 
-ourBoard.connect(Constants.OBCISimulatorPortName) // This will set `simulate` to true
+// Connect to BCI board
+ourBoard.connect(port)
     .then(boardSerial => {
 
         ourBoard.streamStart()
@@ -55,9 +68,10 @@ ourBoard.connect(Constants.OBCISimulatorPortName) // This will set `simulate` to
         ourBoard.on("sample", (sample)=>{
             for (let i = 0; i < ourBoard.numberOfChannels(); i++){
                 let v = sample.channelData[i];
-                if (!vmin || v < vmin) vmin = v;
-                if (!vmax || v > vmax) vmax = v;
-                //console.log(vmin*1000,vmax*1000); [-0.2, 0.2]
+
+                // Update volts range
+                if (!vRange[0] || v < vRange[0]) vRange[0] = v;
+                if (!vRange[1] || v > vRange[1]) vRange[1] = v;
                 
                 //console.log("Channel " + (i + 1) + ": " + v.toFixed(8) + " Volts.");
                 let c = vPrism.encodeChannelValue(v);
